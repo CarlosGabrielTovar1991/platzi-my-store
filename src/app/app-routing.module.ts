f0b65e372603eec5a3fd05b019cd4681aa1af1da
Carlos Gabriel Tovar a.k.a. DarkGabriel1991 <carlosgabrieltovar1991@gmail.com>
2022-09-27 07:08:44 +0200

    Angular routing
    Clase #2 - creando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './pages/category/category.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',
+  component: HomeComponent},
+  {path: 'not-found',
+  component: NotFoundComponent},
+  {path: 'category',
+  component: CategoryComponent},
+  {path: 'my-cart',
+  component: MyCartComponent},
+  {path: 'login',
+  component: LoginComponent},
+  {path: 'register',
+  component: RegisterComponent},
+  {path: 'recovery',
+  component: RecoveryComponent},
+  {path: 'profile',
+  component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
